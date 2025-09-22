9135ead43e1637f15a53e3fc9b901432e1b70b7b
Dancebunny98 <37336183+dancebunny98@users.noreply.github.com>
2025-09-22 04:40:53 +0300

    Update LoginHandler.java

--- src/main/java/com/dreammaster/loginhandler/LoginHandler.java
+++ src/main/java/com/dreammaster/loginhandler/LoginHandler.java
@@ -15,9 +15,7 @@ import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.PlayerEvent;
 
 public class LoginHandler {
-
     // spotless:off
-
     }
     // spotless:on
 }
