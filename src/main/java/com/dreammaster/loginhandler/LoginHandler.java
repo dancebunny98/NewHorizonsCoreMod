9d7bb3d4f8412dc4ad48a462518c3a1075cc3c18
Dancebunny98 <37336183+dancebunny98@users.noreply.github.com>
2025-09-22 04:57:49 +0300

    Update LoginHandler.java

--- src/main/java/com/dreammaster/loginhandler/LoginHandler.java
+++ src/main/java/com/dreammaster/loginhandler/LoginHandler.java
@@ -1,30 +1,14 @@
 package com.dreammaster.loginhandler;
 
-import static com.dreammaster.config.CoreModConfig.ModPackVersion;
-import static net.minecraft.util.EnumChatFormatting.*;
-
-import net.minecraft.event.ClickEvent;
-import net.minecraft.event.HoverEvent;
-import net.minecraft.util.ChatComponentText;
-import net.minecraft.util.ChatStyle;
-import net.minecraft.util.StatCollector;
-
-import com.dreammaster.lib.Refstrings;
-
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.PlayerEvent;
 
 public class LoginHandler {
-    // spotless:off
 
-    // TODO: Add your event handlers or login logic here
-    // Example:
-    /*
+    // spotless:off
     @SubscribeEvent
     public void onPlayerLogin(PlayerEvent.PlayerLoggedInEvent event) {
-        // your code
+        // Сообщения при входе отключены
     }
-    */
-
     // spotless:on
 }
