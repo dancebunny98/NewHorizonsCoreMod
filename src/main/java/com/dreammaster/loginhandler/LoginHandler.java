b9a2c6a38f5ed45ba922e388d9e22c0062385f4d
Dancebunny98 <37336183+dancebunny98@users.noreply.github.com>
2026-02-12 00:50:30 +0300

    Update LoginHandler.java

--- src/main/java/com/dreammaster/loginhandler/LoginHandler.java
+++ src/main/java/com/dreammaster/loginhandler/LoginHandler.java
@@ -1,18 +1,5 @@
 package com.dreammaster.loginhandler;
 
-import static com.dreammaster.config.CoreModConfig.ModPackVersion;
-import static net.minecraft.util.EnumChatFormatting.*;
-
-import net.minecraft.event.ClickEvent;
-import net.minecraft.event.HoverEvent;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.util.ChatComponentText;
-import net.minecraft.util.ChatComponentTranslation;
-import net.minecraft.util.ChatStyle;
-import net.minecraft.util.IChatComponent;
-
-import com.dreammaster.lib.Refstrings;
-
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.PlayerEvent;
 
@@ -21,51 +8,7 @@ public class LoginHandler {
     // spotless:off
     @SubscribeEvent
     public void onPlayerLogin(PlayerEvent.PlayerLoggedInEvent event) {
-        final String WELCOME             = "dreamcraft.welcome.welcome";
-        final String QUESTBOOK           = "dreamcraft.welcome.questbook";
-        final String GTNH_WIKI           = "dreamcraft.welcome.gtnh_wiki";
-        final String CLICK_WIKI          = "dreamcraft.welcome.click_wiki";
-        final String REPORT_BUG          = "dreamcraft.welcome.report_bug";
-        final String CLICK_GITHUB        = "dreamcraft.welcome.click_github";
-        final String VISIT_DISCORD       = "dreamcraft.welcome.visitdiscord";
-        final String CLICK_DISCORD       = "dreamcraft.welcome.click_discord";
-        final String OPEN_TO_LAN_WARNING = "dreamcraft.welcome.open_to_lan";
-
-        final IChatComponent modpackVersion = new ChatComponentText(ModPackVersion).setChatStyle(new ChatStyle().setColor(GREEN));
-
-        final IChatComponent gtnhWikiLink = new ChatComponentText(Refstrings.WIKI_LINK)
-                .setChatStyle(new ChatStyle()
-                        .setChatHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT,
-                                new ChatComponentTranslation(CLICK_WIKI).setChatStyle(new ChatStyle().setColor(YELLOW))))
-                        .setChatClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, Refstrings.WIKI_LINK)));
-
-        final IChatComponent issueTrackerLink = new ChatComponentText(Refstrings.ISSUE_TRACKER_LINK)
-                .setChatStyle(new ChatStyle().setColor(GOLD)
-                        .setChatHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT,
-                                new ChatComponentTranslation(CLICK_GITHUB).setChatStyle(new ChatStyle().setColor(YELLOW))))
-                        .setChatClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, Refstrings.ISSUE_TRACKER_LINK)));
-
-        final IChatComponent discordLink = new ChatComponentText(Refstrings.DISCORD_LINK)
-                .setChatStyle(new ChatStyle()
-                        .setChatHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT,
-                                new ChatComponentTranslation(CLICK_DISCORD).setChatStyle(new ChatStyle().setColor(YELLOW))))
-                        .setChatClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, Refstrings.DISCORD_LINK)));
-
-        event.player.addChatMessage(new ChatComponentText("-----------------------------------------------------")
-                .setChatStyle(new ChatStyle().setColor(GOLD).setStrikethrough(true)));
-        event.player.addChatMessage(new ChatComponentTranslation(WELCOME, modpackVersion).setChatStyle(new ChatStyle().setBold(true)));
-        event.player.addChatMessage(new ChatComponentTranslation(QUESTBOOK).setChatStyle(new ChatStyle().setColor(BLUE)));
-        event.player.addChatMessage(new ChatComponentTranslation(GTNH_WIKI, gtnhWikiLink).setChatStyle(new ChatStyle().setColor(DARK_GREEN)));
-        event.player.addChatMessage(new ChatComponentTranslation(REPORT_BUG).setChatStyle(new ChatStyle().setColor(GREEN)));
-        event.player.addChatMessage(issueTrackerLink);
-        event.player.addChatMessage(new ChatComponentTranslation(VISIT_DISCORD, discordLink).setChatStyle(new ChatStyle().setColor(BLUE)));
-        event.player.addChatMessage(new ChatComponentText("-----------------------------------------------------")
-                .setChatStyle(new ChatStyle().setColor(GOLD).setStrikethrough(true)));
-
-        MinecraftServer server = MinecraftServer.getServer();
-        if (server.isSinglePlayer() && !event.player.getGameProfile().getName().equals(server.getServerOwner())) {
-            event.player.addChatMessage(new ChatComponentTranslation(OPEN_TO_LAN_WARNING));
-        }
+        // Сообщения при входе отключены
     }
     // spotless:on
 }
